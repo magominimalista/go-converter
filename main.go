49b3c57863825c8cb7bd8145eea4531be4f3c7dd
Mago Minimalista <contato@magominimalista.com>
2024-06-27 23:55:03 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var kelvin float64
+	fmt.Print("Digite a temperatura em Kelvin: ")
+	fmt.Scanf("%f", &kelvin)
+
+	celsius := kelvinParaCelsius(kelvin)
+	fmt.Printf("A temperatura em Celsius é: %.2f\n", celsius)
+}
+
+func kelvinParaCelsius(kelvin float64) float64 {
+	return kelvin - 273.15
+}
